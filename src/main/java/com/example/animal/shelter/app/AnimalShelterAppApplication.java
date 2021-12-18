2266f8c68e2a6c7cf431f89c9c2aaa3532f79699
barbaracpm <barbara-ana.moran-ruiz-ramos@capgemini.com>
2021-12-18 10:20:13 +0100

    new springboot project

---
+++ src/main/java/com/example/animal/shelter/app/AnimalShelterAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.animal.shelter.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimalShelterAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnimalShelterAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/animal/shelter/app/AnimalShelterAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.animal.shelter.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimalShelterAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
